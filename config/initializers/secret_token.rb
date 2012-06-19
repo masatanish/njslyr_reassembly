307e95881d6efdd6f9a706404be7a1c0024c3c10
Masata NISHIDA <masatanish@gmail.com>
2012-06-19 23:34:44 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NjslyrReassembly::Application.config.secret_token = '79b82fc73ec417fbe2d56a3612024c5f7a32b2801499b1b31b8712b21000f9a9e2a49260e720b0f8169a80a0479752f962dfaeb5af89db6f75376103ab4955a6'
