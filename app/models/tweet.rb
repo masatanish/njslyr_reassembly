2d8ece5403a5b630a34c37d145feaba4779f4a42
Masata NISHIDA <masatanish@gmail.com>
2012-06-24 19:32:04 +0900

    use tweet_id as model's custom id

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,7 @@ class Tweet
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :_id, type: String, default: ->{ tweet_id }
   field :tweet_id, type: String
   field :text, type: String
   field :posted_at, type: DateTime
